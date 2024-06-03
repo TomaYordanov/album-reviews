c74330b018f5af03d0b0031cfc673ec12d5ed565
TomaYordanov <toma@kibb.org>
2024-06-03 22:44:45 +0300

    commit

---
+++ AlbumReviews/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AlbumReviews.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
