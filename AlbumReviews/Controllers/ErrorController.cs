14072bbb750e8034c84397c7200dbfbf814dc876
TomaYordanov <toma@kibb.org>
2024-06-08 21:44:08 +0300

    added custom error pages

---
+++ AlbumReviews/Controllers/ErrorController.cs
@@ -0,0 +1,26 @@
+﻿using AlbumReviews.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlbumReviews.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [Route("/Error/{statusCode}")]
+        public IActionResult HandleErrorCode(int statusCode)
+        {
+            switch (statusCode)
+            {
+                case 400:
+                    return View("Error400");
+                case 401:
+                    return View("Error401");
+                case 403:
+                    return View("Error403");
+                case 404:
+                    return View("Error404");
+                default:
+                    return View("Error", new ErrorViewModel { StatusCode = statusCode });
+            }
+        }
+    }
+}
---
+++ AlbumReviews/ViewModels/ErrrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace AlbumReviews.ViewModels
+{
+    public class ErrrorViewModel
+    {
+    }
+}
+public class ErrorViewModel
+{
+    public int StatusCode { get; set; }
+    public string RequestId { get; internal set; }
+}
