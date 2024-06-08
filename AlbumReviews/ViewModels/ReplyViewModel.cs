841a256a839928489ad16ae1f067699ceee2bad1
TomaYordanov <toma@kibb.org>
2024-06-08 16:09:31 +0300

    sldkfj

---
+++ AlbumReviews/ViewModels/ReplyViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace AlbumReviews.ViewModels
+{
+    public class ReplyViewModel
+    {
+        public int ReplyId { get; set; }
+        public string ReplyContent { get; set; }
+
+        public string UserName { get; set; }
+
+    }
+}
