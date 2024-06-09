d5447bb96ab3a2b0059e8da29c1a7cac2bec7957
TomaYordanov <toma@kibb.org>
2024-06-09 23:54:07 +0300

    xd

---
+++ AlbumReviews.UnitTests/AlbumTests.cs
@@ -0,0 +1,51 @@
+﻿using AlbumReviews.Data;
+using AlbumReviews.Models;
+using AlbumReviews.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TalkHubAPI.Tests.Repositories;
+
+namespace AlbumReviews.UnitTests
+{
+    public class AlbumTests
+    {
+        private readonly Seeder seeder;
+
+        public AlbumTests()
+        {
+            seeder = new Seeder();
+
+        }
+
+        [Test]
+        public async Task AlbumService_GetAlbumsAsync_ReturnsNotNull()
+        {
+            string name = "IORDAAAAAAAAAAN";
+            AlbumReviewsContext context = seeder.GetDatabaseContext();
+            AlbumService userRepository = new AlbumService(context);
+
+            var result = await userRepository.GetAlbumsAsync();
+
+            Assert.IsNotNull(result); 
+            
+        }
+        [Test]
+        public async Task AlbumService_GetAlbumsQueryable_ReturnsNotNull()
+        {
+            string name = "IORDAAAAAAAAAAN";
+            AlbumReviewsContext context = seeder.GetDatabaseContext();
+            AlbumService userRepository = new AlbumService(context);
+
+            var result = userRepository.GetAlbumsQueryable();
+
+            Assert.IsNotNull(result);
+
+        }
+
+
+
+        }
+    }
---
+++ AlbumReviews.UnitTests/Seeder.cs
@@ -0,0 +1,62 @@
+﻿using AlbumReviews.Data;
+using AlbumReviews.Models;
+using FakeItEasy;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.Text;
+
+
+namespace TalkHubAPI.Tests.Repositories
+{
+    public class Seeder
+    {
+
+
+        public Seeder()
+        {
+
+        }
+
+        public AlbumReviewsContext GetDatabaseContext()
+        {
+            var options = new DbContextOptionsBuilder<AlbumReviewsContext>().EnableSensitiveDataLogging(true)
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            AlbumReviewsContext context = new AlbumReviewsContext(options);
+
+            context.Database.EnsureCreated();
+            if (!context.Users.Any())
+            {
+                User user = new User
+                {
+                    Id = "3800c45c-ae61-4a82-be70-7fb1e8af6aef",
+                    UserName = "test@mail.com",
+                    NormalizedUserName = "TEST@MAIL.COM",
+                    Email = "test@mail.com",
+                    NormalizedEmail = "TEST@MAIL.COM",
+                    PasswordHash = "sometesthash",
+                };
+                context.Add(user);
+                context.SaveChanges();  
+            }
+            if (!context.Albums.Any())
+            {
+                Album user = new Album
+                {
+                    Title = "PISHKA",
+                    Cover = "asd",
+                    Genre = "text",
+                    ReleaseYear = 1,
+                    Artist = "azis"
+                    
+                };
+                context.Add(user);
+                context.SaveChanges();
+            }
+           
+
+            return context;
+        }
+    }
+}
