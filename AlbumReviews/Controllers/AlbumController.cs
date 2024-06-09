69c2dff36bad9687934420f93e77e9a2e17b26f2
TomaYordanov <toma@kibb.org>
2024-06-09 22:28:07 +0300

    updated css #SWAG

--- AlbumReviews/Controllers/AlbumController.cs
+++ AlbumReviews/Controllers/AlbumController.cs
@@ -2,7 +2,6 @@
 using AlbumReviews.Data;
 using AlbumReviews.Models;
 using AlbumReviews.Services;
-using System;
 using System.Linq;
 using System.Threading.Tasks;
 using AlbumReviews.ViewModels;
@@ -12,9 +11,11 @@ namespace AlbumReviews.Controllers
     public class AlbumController : Controller
     {
         private AlbumService _albumService;
-        public async Task<IActionResult> All(string searchString, int pageNumber = 1, int pageSize = 10)
+        public async Task<IActionResult> All(string searchString, string sortOrder, int pageNumber = 1, int pageSize = 10)
         {
             ViewData["CurrentFilter"] = searchString;
+            ViewData["TitleSortParam"] = string.IsNullOrEmpty(sortOrder) ? "title_desc" : "";
+            ViewData["ArtistSortParam"] = sortOrder == "artist" ? "artist_desc" : "artist";
 
             IQueryable<Album> albumsQuery = _albumService.GetAlbumsQueryable();
 
@@ -22,6 +23,22 @@ namespace AlbumReviews.Controllers
             {
                 albumsQuery = albumsQuery.Where(a =>
                     a.Title.Contains(searchString) || a.Artist.Contains(searchString));
+            }
+
+            switch (sortOrder)
+            {
+                case "title_desc":
+                    albumsQuery = albumsQuery.OrderByDescending(a => a.Title);
+                    break;
+                case "artist":
+                    albumsQuery = albumsQuery.OrderBy(a => a.Artist);
+                    break;
+                case "artist_desc":
+                    albumsQuery = albumsQuery.OrderByDescending(a => a.Artist);
+                    break;
+                default:
+                    albumsQuery = albumsQuery.OrderBy(a => a.Title);
+                    break;
             }
 
             var albums = await _albumService.GetAlbumsPagedAsync(albumsQuery, pageNumber, pageSize);
@@ -40,7 +57,7 @@ namespace AlbumReviews.Controllers
 
             return View(paginatedAlbums);
         }
-        public AlbumController (AlbumService albumService)
+        public AlbumController(AlbumService albumService)
         {
             this._albumService = albumService;
         }
